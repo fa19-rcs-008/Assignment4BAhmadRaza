49902cb5ffd02923e296bec2dcfd5fabccb73ef5
DESKTOP-D952LR3\Chaudhary_ahmed <fa19-rcs-008@student.comsats.edu.pk>
2020-07-15 04:10:34 +0500

    Final Code
    Final Code of Assignment4B

---
+++ Assignment4BAhmadRaza/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public GameObject panelmainmenu;
+    public GameObject panelhummingbirds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelmainmenu.SetActive(true);
+    }
+    public void loadPenguingame()
+    {
+        SceneManager.LoadScene("Penguin_Train");
+    }
+    public void loadHummingbirdmenu()
+    {
+        panelhummingbirds.SetActive(true);
+        panelmainmenu.SetActive(false);
+    }
+    public void loadMlAgent()
+    {
+        SceneManager.LoadScene("Training");
+    }
+    public void loadBattlewithmlagent()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+    public void back()
+    {
+        panelmainmenu.SetActive(true);
+        panelhummingbirds.SetActive(false);
+    }
+}
